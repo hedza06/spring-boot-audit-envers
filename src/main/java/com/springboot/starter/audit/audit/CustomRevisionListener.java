055f799b4ce2253345f71a4e5ba2e41d9781af1c
herilmuratovic <heril.muratovic@logate.com>
2018-01-18 13:40:44 +0100

    todo comment changed.

--- src/main/java/com/springboot/starter/audit/audit/CustomRevisionListener.java
+++ src/main/java/com/springboot/starter/audit/audit/CustomRevisionListener.java
@@ -10,6 +10,6 @@ public class CustomRevisionListener implements RevisionListener {
     public void newRevision(Object revisionEntity)
     {
         CustomRevisionEntity entity = (CustomRevisionEntity) revisionEntity;
-        entity.setAuditor("Heril"); // TODO: fetch user from security context and set its username.
+        entity.setAuditor("Heril"); // TODO: fetch user from security context.
     }
 }
